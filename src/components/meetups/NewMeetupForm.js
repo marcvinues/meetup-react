a84a414b9db36c2dc97db4df0ca7385f67c8a407
Marc Vinues <mrdk86@gmail.com>
2024-11-03 19:15:37 +0100

    saving form data and show on the list using localstorage

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,9 +1,38 @@
+import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
 export default function NewMeetupForm() {
+  const [formData, setFormData] = useState({
+    title: "",
+    image: "",
+    address: "",
+    description: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = ({ target: { name, value } }) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
   function submitHandler(event) {
     event.preventDefault();
+    const newFormData = { id: uuidv4(), ...formData };
+    // we have to check localStorage data
+    const existingMeetups = JSON.parse(localStorage.getItem("meetup")) || [];
+    // once we have the data in the way we want, we can update it
+    const updatedMeetups = Array.isArray(existingMeetups)
+      ? [...existingMeetups, newFormData]
+      : [newFormData];
+
+    localStorage.setItem("meetup", JSON.stringify(updatedMeetups));
+    navigate("/");
   }
 
   return (
@@ -11,19 +40,47 @@ export default function NewMeetupForm() {
       <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" required id="title" />
+          <input
+            type="text"
+            required
+            id="title"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Meetup Image</label>
-          <input type="url" required id="image" />
+          <input
+            type="url"
+            required
+            id="image"
+            name="image"
+            value={formData.image}
+            onChange={handleChange}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor="address">Address</label>
-          <input type="text" required id="address" />
+          <input
+            type="text"
+            required
+            id="address"
+            name="address"
+            value={formData.address}
+            onChange={handleChange}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor="description">Description</label>
-          <textarea id="description" required rows="5"></textarea>
+          <textarea
+            id="description"
+            required
+            rows="5"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+          ></textarea>
         </div>
         <div className={classes.actions}>
           <button>Add Meetup</button>
--- src/pages/AllMeetupsPage.js
+++ src/pages/AllMeetupsPage.js
@@ -1,20 +1,32 @@
+import { useEffect, useState } from "react";
 import MeetupItem from "../components/meetups/MeetupItem";
 import classes from "./../components/meetups/MeetupList.module.css";
-
 import { useFetch } from "../util-hooks/useFetch";
 
 export default function AllMeetupsPage() {
-  const { data } = useFetch({
+  const { data: fetchedData } = useFetch({
     url: "/data.json",
   });
 
-  if (!data) return <p>Loading...</p>;
+  const [combinedData, setCombinedData] = useState([]);
+
+  useEffect(() => {
+    if (Array.isArray(fetchedData)) {
+      const localData = JSON.parse(localStorage.getItem("meetup")) || [];
+      const validLocalData = Array.isArray(localData) ? localData : [];
+      const updatedData = [...fetchedData, ...validLocalData];
+
+      setCombinedData(updatedData);
+    }
+  }, [fetchedData]);
+
+  if (!fetchedData) return <p>Loading...</p>;
 
   return (
     <section>
       <h1>All Meetups</h1>
       <ul className={classes.list}>
-        {data.map((item) => (
+        {combinedData.map((item) => (
           <MeetupItem item={item} key={item.id} />
         ))}
       </ul>
